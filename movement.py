0548865323ab46d3c2f99a60402a4f92207a7ba3
Neil Hornus <kingnige@gmail.com>
2018-12-16 02:34:34 +0000

    created array to hold character
    Signed-off-by: Neil Hornus <kingnige@gmail.com>

--- movement.py
+++ movement.py
@@ -1,2 +1,9 @@
-print('hello world')
-print('this is just a test')
+import numpy as np
+
+#create an array to hold the information
+n = 11
+m = 11
+a = [0] * n
+for i in range(n):
+    a[i] = [0] * m
+print(np.matrix(a))
